725802ffb9bc9c6c8892c107e25f8dfe10573b61
Neal Lester <neal@3dsafety.com>
2018-05-10 18:59:09 -0700

    Added sourcery generated LinuxMain.swift.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,7 +1,20 @@
+// Generated using Sourcery 0.13.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
 import XCTest
+@testable import danakeTests
 
-import danakeMongoTests
+extension DanakeMongoTests {
+  static var allTests = [
+    ("testConnection", testConnection),
+    ("testBson", testBson),
+    ("testCount", testCount),
+    ("testDanakeMetadata", testDanakeMetadata),
+    ("testDanakeMongoCreation", testDanakeMongoCreation),
+  ]
+}
 
-var tests = [XCTestCaseEntry]()
-tests += danakeMongoTests.allTests()
-XCTMain(tests)
+
+XCTMain([
+  testCase(DanakeMongoTests.allTests),
+])
