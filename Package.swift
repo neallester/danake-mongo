9722791dbf9275e0a9578c7f9a7431d3f09e862d
Neal Lester <neal@3dsafety.com>
2019-12-21 17:13:01 -0800

    Pinned to a specific version of swift-mongo-driver because newer versions now use a nio based async api. See https://github.com/neallester/danake-mongo/issues/5

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/neallester/danake-sw.git", .branch("master")),
-        .package(url: "https://github.com/mongodb/mongo-swift-driver.git", .branch("master")),
+        .package(url: "https://github.com/mongodb/mongo-swift-driver.git", .revision("be4f099802d023a1847ab091af2cc61e247ac23f")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
